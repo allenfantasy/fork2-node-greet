fc8cfd13a9381f47a22787d1d7fd97625b072073
Allen Wu <allenwu1018@gmail.com>
2014-06-02 10:07:25 +0800

    change src file into coffeescript

--- bin/greet.js
+++ bin/greet.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 var argv = require('minimist')(process.argv.slice(2));
-var greet = require('../index')
+var greet = require('../lib/index')
 //console.log("Hello world");
 console.log(greet(argv._[0], argv.drunk))
